1490bfe6a96f84c4de63f2429cf29dadb3d9877e
lucas papp <lucas.papp007@gmail.com>
2026-07-08 20:06:58 -0300

    Base API
    Se creo el codigo base de la API con funciones basicas para retornar y recibir datos de los sensores

--- api/app/main.py
+++ api/app/main.py
@@ -1,7 +1,45 @@
 from fastapi import FastAPI
-
+from pydantic import BaseModel
+from datetime import datetime
 app = FastAPI()
+datos_sensores = []
+
+class SensorData (BaseModel):
+    temperatura: float
+    viento: float
+    humedad: float
+    aire: float
 
 @app.get("/")
-def root():
-	return {"mensaje": "Servidor API funcionando"}
+def inicio():
+    return {
+        "mensaje": "Servidor funcionando"
+    }
+
+# Aca envia los datos la ESP32
+@app.post("/sensores")
+def recibirDatos(datos: SensorData):
+    registro = {
+        "temperatura": datos.temperatura,
+        "viento": datos.viento,
+        "humedad": datos.humedad,
+        "aire": datos.aire,
+        "fecha": datetime.now().strftime("%H:%M:%S")
+    }
+    datos_sensores.append(registro)
+    print('Datos Recibidos')
+    print(registro)
+    return {
+        "ok": True,
+        "mensaje": "Datos Guardados"
+    }
+
+# Devuelve el ultimo informe recibido
+@app.get("/sensores/actual")
+def sensoresActual():
+    if len(datos_sensores) == 0:
+        return {
+            "mensaje": "No hay datos aun"
+        }
+    
+    return datos_sensores[-1]
